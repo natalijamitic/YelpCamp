90aaf7ae6e49fcf7590224f349d27531f5e0b607
Natalija Mitic <naca@ntec.ch>
2019-10-04 23:48:25 +0200

    Campground Authorization, only authors can edit/delete campgrounds

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -52,17 +52,13 @@ router.get("/:id", (req, res) => {
 });
 
 // EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", (req, res) => {
+router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
   Campground.findById(req.params.id, (err, foundCampground) => {
-    if (err) {
-      res.redirect("/campgrounds");
-    } else {
       res.render("campgrounds/edit", {campground: foundCampground});
-    }
   });
 });
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id", (req, res) => {
+router.put("/:id", checkCampgroundOwnership, (req, res) => {
   // find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
     if (err) {
@@ -75,7 +71,7 @@ router.put("/:id", (req, res) => {
 });
 
 // DESTROY CAMPGROUND ROUTE
-router.delete("/:id", (req, res) => {
+router.delete("/:id", checkCampgroundOwnership, (req, res) => {
   Campground.findByIdAndRemove(req.params.id, (err, campgroundRemoved) => {
     if (err) {
       res.redirect("/campgrounds");
@@ -90,12 +86,33 @@ router.delete("/:id", (req, res) => {
   });
 });
 
+
 //middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+      if (err) {
+        res.redirect("back");
+      } else {
+          //does user own campground
+          if (foundCampground.author.id.equals(req.user._id)) {
+            next();
+          } else {
+            res.redirect("back");
+          }
+
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
